7cc515c26effcd0914785ba6d04ff81a734837fa
mdakashdeveloper <mdakashdeveloper@gmail.com>
2025-07-02 12:31:12 +0600

    change

--- packages/auth-service/src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './auth.module.js';
-export * from './auth.service.js';
-export * from './guards/index.js';
-export * from './strategies/index.js';
-export * from './dto/index.js';
-export * from './interfaces/index.js';
--- packages/local-app/src/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './local-database.js';
-export * from './sync-manager.js';
-export * from './plugin-system.js';
-export * from './storage-manager.js';
-export * from './backup-restore.js';
-export * from './feature-flags.js';
-export * from './interfaces/index.js';
